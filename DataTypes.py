f57bd68b327e23bb269e27d47b9f4530130d2de6
angegon <angegon@gmail.com>
2021-02-07 22:27:38 +0100

    0.1

---
+++ DataTypes.py
@@ -0,0 +1,3 @@
+print(type("Hola"))
+print(type(2))
+print(type(2.5))
---
+++ hello.py
@@ -0,0 +1,17 @@
+friends = ['Taylor', 'Alex', 'Pat', 'Eli']
+for friend in friends:
+    print("Hi " + friend)
+
+for i in range(10):
+  print("Hello, World!")
+
+print("I'm programming in Python!" )
+
+color = "Green"
+thing = "Hope"
+print(color + " is the color of " + thing)
+
+print((((1+2)*3)/4)**5)
+
+ratio = 1 + (5**(1/2))
+print(ratio)
