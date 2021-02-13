caee831b0dd6e0ecc396ad2453ec8ef254e53a23
angegon <angegon@gmail.com>
2021-02-13 15:14:46 +0100

    0.2

--- conditionals.py
+++ conditionals.py
@@ -26,6 +26,33 @@ print(1 == "1")
 # and, or, != (not)
 
 #Branching with if Statements if, elif, else
+"""Conditionals Cheat Sheet
+
+Comparison operators
+a == b: a is equal to b
+a != b: a is different than b
+a < b: a is smaller than b
+a <= b: a is smaller or equal to b
+a > b: a is bigger than b
+a >= b: a is bigger or equal to b
+Logical operators
+a and b: True if both a and b are True. False otherwise.
+a or b: True if either a or b or both are True. False if both are False.
+not a: True if a is False, False if a is True.
+Branching blocks
+
+In Python, we branch our code using if, else and elif. This is the branching syntax:
+
+if condition1:
+	if-block
+elif condition2:
+	elif-block
+else:
+	else-block
+
+Remember: The if-block will be executed if condition1 is True. The elif-block will
+ be executed if condition1 is False and condition2 is True. The else block will
+  be executed when all the specified conditions are false."""
 
 def is_positive(number):
   if number > 0:
@@ -57,4 +84,92 @@ def number_group(number):
 print(number_group(10)) #Should be Positive
 print(number_group(0)) #Should be Zero
 print(number_group(-5)) #Should be Negative
+
+
+def color_translator(color):
+	if color == "red":
+		hex_color = "#ff0000"
+	elif color == "green":
+		hex_color = "#00ff00"
+	elif color == "blue":
+		hex_color = "#0000ff"
+	else:
+		hex_color = "unknown"
+	return hex_color
+
+
+def exam_grade(score):
+	if score > 96:
+		grade = "Top Score"
+	elif score >= 60:
+		grade = "Pass"
+	else:
+		grade = "Fail"
+	return grade
+
+print(exam_grade(65)) # Should be Pass
+print(exam_grade(55)) # Should be Fail
+print(exam_grade(60)) # Should be Pass
+print(exam_grade(95)) # Should be Pass
+print(exam_grade(100)) # Should be Top Score
+print(exam_grade(0)) # Should be Fail
+
+
+def sum(x, y):
+		return(x+y)
+print(sum(sum(1,2), sum(3,4)))
+
+
+def format_name(first_name, last_name):
+	# code goes here
+	if first_name != "" and last_name != "":
+		string = "Name: " + last_name + " " + first_name
+	elif first_name != "":
+		string = "Name: " + first_name
+	elif last_name != "":
+		string = "Name: " + last_name
+	else:
+		string = ""
+	return string 
+
+print(format_name("Ernest", "Hemingway"))
+# Should return the string "Name: Hemingway, Ernest"
+
+print(format_name("", "Madonna"))
+# Should return the string "Name: Madonna"
+
+print(format_name("Voltaire", ""))
+# Should return the string "Name: Voltaire"
+
+print(format_name("", ""))
+# Should return an empty string
+
+
+def longest_word(word1, word2, word3):
+	if len(word1) >= len(word2) and len(word1) >= len(word3):
+		word = word1
+	elif len(word2) >= len(word3):
+		word = word2
+	else:
+		word = word3
+	return(word)
+
+print(longest_word("chair", "couch", "table"))
+print(longest_word("bed", "bath", "beyond"))
+print(longest_word("laptop", "notebook", "desktop"))
+
+
+def fractional_part(numerator, denominator):
+	# Operate with numerator and denominator to 
+	# keep just the fractional part of the quotient
+	if denominator != 0:
+		return abs(numerator/denominator) - abs(int(numerator/denominator))
+	return 0
+
+print(fractional_part(5, 5)) # Should be 0
+print(fractional_part(5, 4)) # Should be 0.25
+print(fractional_part(5, 3)) # Should be 0.66...
+print(fractional_part(5, 2)) # Should be 0.5
+print(fractional_part(5, 0)) # Should be 0
+print(fractional_part(0, 5)) # Should be 0
 
